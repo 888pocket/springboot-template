d84ba39878efbb0061bdd709eedad7d0f40b4e74
888pocket <dndmdo8@gmail.com>
2024-02-22 15:19:43 +0900

    feat: custom exception 추가

---
+++ web-full-stack/src/main/java/com/example/webfullstack/common/exception/CustomException.java
@@ -0,0 +1,8 @@
+package com.example.webfullstack.common.exception;
+
+public class CustomException extends RuntimeException {
+
+    public CustomException() {
+        super("Custom exception");
+    }
+}
